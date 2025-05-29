46c03f467dccd675e871dfec3ea12b6aa3a8aab2
valoriexwz <valoryxiaowen@gmail.com>
2025-05-29 17:48:12 +0800

    updated dtcpay work

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,13 @@
   $('#preloader').fadeOut();
 
   // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 50) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
-    });
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() > 50) {
+      $('.scrolling-navbar').addClass('top-nav-collapse');
+    } else {
+      $('.scrolling-navbar').removeClass('top-nav-collapse');
+    }
+  });
 
     // one page navigation 
     $('.navbar-nav').onePageNav({
@@ -26,23 +26,23 @@
     /* Auto Close Responsive Navbar on Click
     ========================================================*/
     function close_toggle() {
-        if ($(window).width() <= 768) {
-            $('.navbar-collapse a').on('click', function () {
-                $('.navbar-collapse').collapse('hide');
-            });
-        }
-        else {
-            $('.navbar .navbar-inverse a').off('click');
-        }
+      if ($(window).width() <= 768) {
+        $('.navbar-collapse a').on('click', function () {
+          $('.navbar-collapse').collapse('hide');
+        });
+      }
+      else {
+        $('.navbar .navbar-inverse a').off('click');
+      }
     }
     close_toggle();
     $(window).resize(close_toggle);
 
     /* WOW Scroll Spy
     ========================================================*/
-     var wow = new WOW({
+    var wow = new WOW({
       //disabled for mobile
-        mobile: false
+      mobile: false
     });
 
     wow.init();
@@ -56,82 +56,76 @@
     
 
      /* Testimonials Carousel 
-    ========================================================*/
-    var owl = $("#testimonials");
-      owl.owlCarousel({
-        loop: true,
-        nav: false,
-        dots: true,
-        center: true,
-        margin: 15,
-        slideSpeed: 1000,
-        stopOnHover: true,
-        autoPlay: true,
-        responsiveClass: true,
-        responsiveRefreshRate: true,
-        responsive : {
-            0 : {
-                items: 1
-            },
-            768 : {
-                items: 2
-            },
-            960 : {
-                items: 3
-            },
-            1200 : {
-                items: 3
-            },
-            1920 : {
-                items: 3
-            }
+     ========================================================*/
+     var owl = $("#testimonials");
+     owl.owlCarousel({
+      loop: true,
+      nav: false,
+      dots: true,
+      center: true,
+      margin: 15,
+      slideSpeed: 1000,
+      stopOnHover: true,
+      autoPlay: true,
+      responsiveClass: true,
+      responsiveRefreshRate: true,
+      responsive : {
+        0 : {
+          items: 1
+        },
+        768 : {
+          items: 2
+        },
+        960 : {
+          items: 3
+        },
+        1200 : {
+          items: 3
+        },
+        1920 : {
+          items: 3
         }
-      }); 
+      }
+    }); 
 
       // navscroll disappear and appear 
       // =============
 
-     var lastScrollTop = 0;
-     
+      var lastScrollTop = 0;
+      
 
-$(window).scroll(function () {
-  
-var st = $(this).scrollTop();
-var sd = lastScrollTop-st;
+      $(window).scroll(function () {
+        
+        var st = $(this).scrollTop();
+        var sd = lastScrollTop-st;
 
         if (st < lastScrollTop && sd > 40 || st==0 ){
-            $('.navbar ').fadeIn( 'slow' );
+          $('.navbar ').fadeIn( 'slow' );
         } else if(st > lastScrollTop && sd < -20) {
           $('.navbar ').fadeOut( 'slow' );
         }
         lastScrollTop = st;
-  })
-
-
-
-
-
-      
+      })
 
 
     /* Back Top Link active
     ========================================================*/
-      var offset = 200;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
+    var offset = 200;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
+    $('.back-to-top').on('click',function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    });
 
   });      
 
@@ -145,32 +139,32 @@ var sd = lastScrollTop-st;
 // highlight
 
 $(".highlight-link").each(function(){
-        if ( $(this).isOnScreenHighlight() ) {
-              $(this).addClass('shown');
-            } else {
-              $(this).removeClass('shown');
-        }
+  if ( $(this).isOnScreenHighlight() ) {
+    $(this).addClass('shown');
+  } else {
+    $(this).removeClass('shown');
+  }
+});
+
+
+$(window).scroll(function(){
+  $(".highlight").each(function(){
+    if ( $(this).isOnScreenHighlight() ) {
+      $(this).addClass('shown');
+    } else {
+      $(this).removeClass('shown');
+    }
   });
 
-
-  $(window).scroll(function(){
-    $(".highlight").each(function(){
-          if ( $(this).isOnScreenHighlight() ) {
-            $(this).addClass('shown');
-              } else {
-                  $(this).removeClass('shown');
-          }
-    });
-
-    $(".highlight-link").each(function(){
-          if ( $(this).isOnScreenHighlight() ) {
-                  $(this).css("animation-delay","0s");
-                  $(this).addClass('shown');
-              } else {
-                $(this).removeClass('shown');
-          }
-    });
+  $(".highlight-link").each(function(){
+    if ( $(this).isOnScreenHighlight() ) {
+      $(this).css("animation-delay","0s");
+      $(this).addClass('shown');
+    } else {
+      $(this).removeClass('shown');
+    }
   });
+});
 
 $.fn.isOnScreenHighlight = function(){
 
@@ -178,8 +172,8 @@ $.fn.isOnScreenHighlight = function(){
   var win = $(window);
 
   var viewport = {
-      top : win.scrollTop(),
-      left : win.scrollLeft()
+    top : win.scrollTop(),
+    left : win.scrollLeft()
   };
   viewport.right = viewport.left + win.width();
   viewport.bottom = viewport.top + win.height();
@@ -192,58 +186,85 @@ $.fn.isOnScreenHighlight = function(){
 
 };
 
+const correctPassword = "valzhang";
+let currentLink = "";
+
+document.querySelectorAll(".popup-trigger")
+.forEach(btn =>
+  btn.addEventListener("click", e => {
+      // stash the link
+      currentLink = e.currentTarget.dataset.link;
+      // reset to hidden state on open:
+      pwdInput.type = "password";
+      iconImg.src  = ICON_OFF;
+      iconImg.alt  = "Show password";
+      // show the modal
+      const modal = document.getElementById("password-modal");
+      modal.classList.add("is-visible");
+      // auto-focus the input
+      document.getElementById("password-input").focus();
+    })
+  );
+
+
+document.getElementById("password-submit")
+.addEventListener("click", () => {
+ const pwd = document.getElementById("password-input").value;
+ if (pwd === correctPassword) {
+  window.open(currentLink, "_blank");
+  closeModal();
+} else {
+ alert("Incorrect password. Please contact Val.");
+}
+});
 
 
 
-// contact-dialog pop-up
+// Paths to your icons
+const ICON_ON  = "assets/img/home/visible_on.svg";
+const ICON_OFF = "assets/img/home/visible_off.svg";
 
-const dialog_button = document.getElementById("contact-dialog");
-const image_click_trigger = document.getElementById("image-click-trigger");
-var modal = document.getElementById("myModal");
+const pwdInput = document.getElementById("password-input");
+const toggleBtn = document.getElementById("toggle-password");
+const iconImg   = document.getElementById("password-icon");
 
-// Password to access the link
-  const correctPassword = "valzhang";
-
-  function checkPassword() {
-    const passwordInput = document.getElementById("passwordInput").value;
-
-    if (passwordInput === correctPassword) {
-      // Password is correct, open the link
-       window.open("https://www.figma.com/board/NA8TuxSox3d8cE62WTHpOp/Val-2024?node-id=0-1&t=tyesz6XaFatRS0HA-1", "_blank"); 
-    } else {
-      alert("Incorrect password. Please contact Val.");
-    }
-  }
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("cd-popup-close")[0];
-
-// if the dialog is clicked
+toggleBtn.addEventListener("click", () => {
+  const isHidden = pwdInput.type === "password";
+  // flip input type
+  pwdInput.type = isHidden ? "text" : "password";
+  // swap icon and alt text
+  iconImg.src = isHidden ? ICON_ON : ICON_OFF;
+  iconImg.alt = isHidden ? "Hide password" : "Show password";
+});
 
 
-dialog_button.onclick = function() {
-  console.log("clikced");
-  $('.cd-popup').addClass('is-visible');
+
+// 4) modal close logic
+function closeModal() {
+  document.getElementById("password-modal")
+  .classList.remove("is-visible");
+  document.getElementById("password-input").value = "";
+  currentLink = "";
 }
 
-// Event listener for the image click
- if (image_click_trigger) {image_click_trigger.onclick = function() {
-  console.log("Image clicked");
-  $('.cd-popup').addClass('is-visible');
-}}
+// close on “×”
+document.querySelector(".cd-popup-close")
+.addEventListener("click", closeModal);
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    $('.cd-popup').removeClass('is-visible');
-  }
-}
+// close on outside click
+window.addEventListener("click", e => {
+  if (e.target.id === "password-modal") closeModal();
+});
 
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  $('.cd-popup').removeClass('is-visible');
-}
 
+// fire the “Show me” click when Enter is pressed
+pwdInput.addEventListener("keydown", e => {
+  if (e.key === "Enter") {
+    e.preventDefault();                       // prevent any default form submission
+    document.getElementById("password-submit")
+            .click();                         // reuse your existing click handler
+          }
+        });
 
 
